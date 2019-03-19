e9287b604727de4c8ed068e71d1b4c5188dbc48d
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-03-19 17:14:13 +0100

    detail and main classes implemented

---
+++ app/src/main/java/com/example/mainphoneapp/DetailActivity.java
@@ -0,0 +1,39 @@
+package com.example.mainphoneapp;
+
+
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.io.Serializable;
+
+import com.example.mainphoneapp.Model.BEFriend;
+
+public class DetailActivity extends AppCompatActivity {
+     String TAG = MainActivity.TAG;
+
+     EditText etName;
+     EditText etPhone;
+     CheckBox cbFav;
+
+     @Override
+        protected void onCreate(Bundle savedInstanceState){
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_detail);
+         Log.d(TAG, " Detail activity is running");
+
+         etName = findViewById(R.id.etName);
+         etPhone = findViewById(R.id.etPhone);
+         cbFav = findViewById(R.id.cbFavorite);
+     }
+
+     private void setGui(){
+         BEFriend friend = (BEFriend) getIntent().getSerializableExtra("friend");
+         etName.setText(friend.getName());
+         etPhone.setText(friend.getPhone());
+         cbFav.setChecked(friend.isFavorite());
+     }
+}
--- app/src/main/java/com/example/mainphoneapp/MainActivity.java
+++ app/src/main/java/com/example/mainphoneapp/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.mainphoneapp;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.mainphoneapp.Model.BEFriend;
+import com.example.mainphoneapp.Model.Friends;
+
+public class MainActivity extends ListActivity {
+
+    public static String TAG = "MainPhoneApp";
+
+    Friends m_friends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        this.setTitle("Main Phone App");
+        m_friends = new Friends();
+
+        String[] friends;
+
+        friends = m_friends.getNames();
+
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, friends);
+
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView parent, View v, int position, long id){
+        Intent detailIntent = new Intent(this, DetailActivity.class );
+        Log.d(TAG, "We start the detail activity here");
+        BEFriend friend = m_friends.getAll().get(position);
+        addData(detailIntent, friend);
+        startActivity(detailIntent);
+        Log.d(TAG, "Detail activity is now running");
+    }
+
+    private void addData(Intent detailIntent, BEFriend friend){
+        detailIntent.putExtra("friend", friend);
     }
 }
