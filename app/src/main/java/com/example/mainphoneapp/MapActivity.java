30898a5b3d56a0d6d8b63f6a2d1bd0ff837c406f
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-03-21 17:08:05 +0100

    fixed stupid abstract class bug - gradle implementation fixed - no more red

--- app/src/main/java/com/example/mainphoneapp/MapActivity.java
+++ app/src/main/java/com/example/mainphoneapp/MapActivity.java
@@ -10,20 +10,16 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public abstract class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity {
 
     private static String LOGTAG = "MAP_LOG";
 
