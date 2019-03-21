537a41788a290166faf7ae873ef965eafa8ecc12
langehk <rupkri32@gmail.com>
2019-03-21 13:34:56 +0100

    build.gradle

--- app/src/main/java/com/example/mainphoneapp/MapActivity.java
+++ app/src/main/java/com/example/mainphoneapp/MapActivity.java
@@ -6,7 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MapActivity extends AppCompatActivity {
+
+
+    private GoogleMap mMap;
+
+    private final LatLng EASV = new LatLng(55.488230, 8.446936);
 
 
     @Override
@@ -14,17 +31,35 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+        //Button back from map
         Button btnBackFromMap = findViewById(R.id.btnBackFromMap);
 
 
-        btnBackFromMap.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v)
-            {
+        btnBackFromMap.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 onClickBackToDetail();
             }
         });
 
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
+
 
 
     private void onClickBackToDetail() {
