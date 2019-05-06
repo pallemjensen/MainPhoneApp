388765e24150a2a6feacb2fc600f66013710dd59
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-05-06 15:43:27 +0200

    setter and getters done except location

--- app/src/main/java/com/example/mainphoneapp/Model/BEFriend.java
+++ app/src/main/java/com/example/mainphoneapp/Model/BEFriend.java
@@ -13,65 +13,80 @@ public class BEFriend implements Serializable {
     private String m_picture;
     private String m_address;
 
-    public BEFriend(String id, String name, String phone, Double lat, Double lon, String mail, String picture,  String address) {
-        this.m_id = id;
-        this.m_name = name;
-        this.m_phone = phone;
-        this.m_lat = lat;
-        this.m_lon = lon;
-        this.m_mail = mail;
-        this.m_picture = picture;
-        this.m_address = address;
-    }
 
-    public BEFriend(String name, String phone, Double lat, Double lon, String mail, String picture, String address) {
-        this.m_name = name;
-        this.m_phone = phone;
-        this.m_lat = lat;
-        this.m_lon = lon;
-        this.m_mail = mail;
-        this.m_picture = picture;
-        this.m_address = address;
-    }
-
-    public BEFriend(){
+    public BEFriend() {
     }
 
     public String getPhone() {
         return m_phone;
     }
 
+    public void setPhone(String phone) {
+        this.m_phone = phone;
+    }
+
     public String getName() {
         return m_name;
     }
 
-    public Double getLon(){ return m_lon;}
+    public void setName(String name) {
+        this.m_name = name;
+    }
 
-    public Double getLat() {return m_lat;}
+    public Double getLon() {
+        return m_lon;
+    }
+
+    public Double getLat() {
+        return m_lat;
+    }
 
     public void setLat(Double lat) {
         this.m_lat = lat;
     }
 
-    public void setLng(Double lng){
+    public void setLng(Double lng) {
         this.m_lon = lng;
     }
 
-    public String getMail() {return m_mail;}
-
-    public String getPicture() {return m_picture;}
-
-    public void setPicture(String picture) {this.m_picture = picture;}
-
-
-    public String getAddress() {return m_address;}
-
-    public String toString() {return "" + m_name;}
-
-    public String getId() {return m_id;}
-
-    public void setId(String id) {this.m_id = id;}
+    public String getMail() {
+        return m_mail;
     }
 
+    public void setmail(String mail) {
+        this.m_mail = mail;
+    }
+
+    public String getPicture() {
+        return m_picture;
+    }
+
+    public void setPicture(String picture) {
+        this.m_picture = picture;
+    }
+
+
+    public String getAddress() {
+        return m_address;
+    }
+
+    public void setAddress(String address) {
+        this.m_address = address;
+    }
+
+    public String getId() {
+        return m_id;
+    }
+
+    public void setId(String id) {
+        this.m_id = id;
+    }
+
+
+    public String toString() {
+        return "" + m_name;
+    }
+
+}
 
 
